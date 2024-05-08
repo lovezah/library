d7a461757afed4d8bb9878088a3f57f42894c775
lovezah <zah0109@126.com>
2024-05-08 17:32:57 +0800

    test union_find

---
+++ test/library_checker/datastructure/union_find.test.cpp
@@ -0,0 +1,18 @@
+#include <misc/my_template.hpp>
+#include <ds/union_find.hpp>
+
+void solve() {
+  ints(n, q);
+  union_find uf(n);
+  rep(q) {
+    ints(t, u, v);
+    if (t == 0) {
+      uf.merge(u, v);
+    } else print(uf[u] == uf[v] ? 1 : 0);
+  }
+}
+
+int main() {
+  SETIO();
+  solve();
+}
