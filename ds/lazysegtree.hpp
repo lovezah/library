fd47f1a3f5d4f1cfe29f142f007ed6f217be3f61
lovezah <zah0109@126.com>
2024-06-11 04:33:23 +0800

    add seg

--- ds/lazysegtree.hpp
+++ ds/lazysegtree.hpp
@@ -1,16 +1,18 @@
-template <class X, X(* op)(X, X), X (*e)()>
-struct segtree {
+template <class X, X(* op)(X, X), X (*e)(), class F, X (*mapping)(X, F), F (*composition)(F, F), F (*id)()>
+struct lazysegtree {
   int n;
   vector<X> v;
-  segtree(int m = 0) {
+  vector<F> lz;
+  lazysegtree(int m = 0) {
     init(vector<X>(m, e()));
   }
-  segtree(const vector<X> &dat) {
+  lazysegtree(const vector<X> &dat) {
     init(dat);
   }
   void init(vector<X> dat) {
     n = len(dat);
     v.assign(4 << (int)(log2(n)), e());
+    lz.assign(4 << (int)(log2(n)), id());
     function<void(int, int, int)> build = [&](int p, int l, int r) {
       if (r - l == 1) {
         v[p] = dat[l];
@@ -26,30 +28,54 @@ struct segtree {
   void pull(int p) {
     v[p] = op(v[p * 2], v[p * 2 + 1]);
   }
+  void all_apply(int p, F f) {
+    v[p] = mapping(v[p], f);
+    lz[p] = composition(lz[p], f);
+  }
+  void push(int p) {
+    all_apply(p * 2, lz[p]);
+    all_apply(p * 2 + 1, lz[p]);
+    lz[p] = id();
+  }
   void set(int p, int l, int r, int x, X c) {
     if (r - l == 1) {
       v[p] = c;
       return;
     }
+    push(p);
     int m = (l + r) / 2;
     if (x < m) set(p * 2, l, m, x, c);
     else set(p * 2 + 1, m, r, x, c);
     pull(p);
   }
-  void add(int p, int l, int r, int x, X c) {
+  void apply(int p, int l, int r, int x, F f) {
     if (r - l == 1) {
-      v[p] += c;
+      all_apply(p, f);
       return;
     }
+    push(p);
     int m = (l + r) / 2;
-    if (x < m) add(p * 2, l, m, x, c);
-    else add(p * 2 + 1, m, r, x, c);
+    if (x < m) apply(p * 2, l, m, x, f);
+    else apply(p * 2 + 1, m, r, x, f);
+    pull(p);
+  }
+  void apply(int p, int l, int r, int ql, int qr, F f) {
+    if (qr <= l || r <= ql) return;
+    if (ql <= l && r <= qr) {
+      all_apply(p, f);
+      return;
+    }
+    push(p);
+    int m = (l + r) / 2;
+    apply(p * 2, l, m, ql, qr, f);
+    apply(p * 2 + 1, m, r, ql, qr, f);
     pull(p);
   }
   X get(int p, int l, int r, int x) {
     if (r - l == 1) {
       return v[p];
     }
+    push(p);
     int m = (l + r) / 2;
     if (x < m) return get(p * 2, l, m, x);
     else return get(p * 2 + 1, m, r, x);
@@ -57,6 +83,7 @@ struct segtree {
   X query(int p, int l, int r, int ql, int qr) {
     if (qr <= l || r <= ql) return e();
     if (ql <= l && r <= qr) return v[p];
+    push(p);
     int m = (l + r) / 2;
     return op(query(p * 2, l, m, ql, qr), query(p * 2 + 1, m, r, ql, qr));
   }
@@ -65,6 +92,7 @@ struct segtree {
     if (qr <= l || r <= ql) return -1;
     if (ql <= l && r <= qr && !f(v[p])) return -1;
     if (r - l == 1) return l;
+    push(p);
     int m = (l + r) / 2;
     int res = find_first(p * 2, l, m, ql, qr, f);
     if (res == -1) {
@@ -77,6 +105,7 @@ struct segtree {
     if (qr <= l || r <= ql) return -1;
     if (ql <= l && r <= qr && !f(v[p])) return -1;
     if (r - l == 1) return l;
+    push(p);
     int m = (l + r) / 2;
     int res = find_last(p * 2 + 1, m, r, ql, qr, f);
     if (res == -1) {
@@ -88,10 +117,14 @@ struct segtree {
   void set(int p, X x) {
     assert(0 <= p && p < n);
     set(1, 0, n, p, x);
-  }  
-  void add(int p, X x) {
+  }
+  void apply(int p, F f) {
     assert(0 <= p && p < n);
-    add(1, 0, n, p, x);
+    apply(1, 0, n, p, f);
+  }
+  void apply(int l, int r, F f) {
+    assert(0 <= l && l < r && r <= n);
+    apply(1, 0, n, l, r, f);
   }
   X get(int p) {
     assert(0 <= p && p < n);
