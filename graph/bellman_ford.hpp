f88e4214f9dd22fead704408d02cc074faa40bec
lovezah <zah0109@126.com>
2024-07-07 00:21:01 +0800

    add BF

---
+++ graph/bellman_ford.hpp
@@ -0,0 +1,36 @@
+/*
+  source: template from kactl
+  link  : https://github.com/kth-competitive-programming/kactl/blob/main/content/graph/BellmanFord.h
+  docs  : Unreachable nodes get dist = inf; nodes reachable through negative-weight cycles get dist = -inf.
+*/
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+const ll inf = LLONG_MAX;
+struct Ed { int a, b, w; int s() { return a < b ? a : -a; }};
+struct Node { ll dist = inf; int prev = -1; };
+
+void bellman_ford(vector<Node>& nodes, vector<Ed>& eds, int s) {
+	nodes[s].dist = 0;
+	sort(all(eds), [](Ed a, Ed b) { return a.s() < b.s(); });
+
+	int lim = sz(nodes) / 2 + 2; // /3+100 with shuffled vertices
+	rep(i,0,lim) for (Ed ed : eds) {
+		Node cur = nodes[ed.a], &dest = nodes[ed.b];
+		if (abs(cur.dist) == inf) continue;
+		ll d = cur.dist + ed.w;
+		if (d < dest.dist) {
+			dest.prev = ed.a;
+			dest.dist = (i < lim-1 ? d : -inf);
+		}
+	}
+	rep(i,0,lim) for (Ed e : eds) {
+		if (nodes[e.a].dist == -inf)
+			nodes[e.b].dist = -inf;
+	}
+}
