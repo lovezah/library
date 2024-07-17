784996bc2eb8234336cbd79fe922d2b99496c4b2
lovezah <zah0109@126.com>
2024-07-17 16:10:41 +0800

    add gl.hpp

---
+++ misc/gl.hpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define rep(i,a,n) for(int i=a;i<n;i++)
+#define all(x) (x).begin(),(x).end()
+#define sz(x) (int)(x).size()
+using ll=long long;
+using vi=vector<int>;
+using pi=pair<int,int>;
+const char nl='\n';
+
+int main(){
+  cin.tie(0)->sync_with_stdio(0);
+
+}
